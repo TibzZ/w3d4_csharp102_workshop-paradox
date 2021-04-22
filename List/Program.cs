5c954b7fad8747226bba6665cf57c193a6a14336
Tibz <thibaut_marechal@yahoo.com>
2021-04-22 21:50:41 +0100

    Task 1 start, create alist and loop through names

---
+++ List/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListTest
+{
+    class Program
+    {
+        static void Main(string[] args){
+        
+            List<string> namesCoaches = new List<string>();
+            namesCoaches.Add("James");
+            namesCoaches.Add("Ben");
+            namesCoaches.Add("Liz");
+            namesCoaches.Add("Tao");
+            namesCoaches.Add("Chris");
+            Console.WriteLine("namesCoaches");
+            namesCoaches.ForEach(name => Console.Write("{0}\t", name));
+            
+        }
+    }
+}
