17810ba4aec3686f1c875053ad85b67d2cb2e127
Tibz <thibaut_marechal@yahoo.com>
2021-04-26 09:34:15 +0100

    Task 5 complete, Linq to be done in next lesson

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions; 
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // TASK 5 - Create a Fibonacci List
+            List<int> FibonacciNos = new List<int>{1,1};
+            
+            
+            // Create 20 iteration to add a new number following Fibonacci paradime
+            for(int i = 0; i <= 20; i++)
+            { 
+                int lastElementFiboNo = FibonacciNos[FibonacciNos.Count -1];
+                int penultimateFiboNo = FibonacciNos[FibonacciNos.Count -2];
+                FibonacciNos.Add(penultimateFiboNo + lastElementFiboNo);
+            }           
+            
+            //Display results 
+            foreach(int fibNo in FibonacciNos)
+            {
+                Console.WriteLine(fibNo);
+            }
+
+            
+        }
+    }
+}
